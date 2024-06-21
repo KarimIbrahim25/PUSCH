26a0707bd048af40802846b080b1fe21f170811d
ZeyadNar <zeyadnar88@gmail.com>
2024-06-21 08:33:11 +0300

    Update Scramber.v
    EN removed

--- Scrambler/Scramber.v
+++ Scrambler/Scramber.v
@@ -2,7 +2,6 @@ module Scrambler(
     
     input wire CLK_SC , 
     input wire RST_SC , 
-    input wire EN_SC , 
     input wire SC_IN , // input bit from interleaver
     input wire GOLD_VALID , //Valid signal from PR_Gen 
     input wire SC_BUSY_IN , 
@@ -18,15 +17,9 @@ always @ (posedge CLK_SC or negedge RST_SC) begin
         SC_Valid_OUT <= 'b0 ; 
         SC_OUT <= 0 ; 
     end   
-    else if (EN_SC) begin
-        if(SC_Valid_IN && GOLD_VALID && SC_BUSY_IN) begin 
+    else if(SC_Valid_IN && GOLD_VALID && SC_BUSY_IN) begin 
             SC_OUT <= Gold_IN ^ SC_IN ;
             SC_Valid_OUT <= 1'b1 ; 
-        end
-        else begin 
-             SC_OUT <= 0 ;
-             SC_Valid_OUT <= 1'b0 ; 
-        end
     end
     else begin 
         SC_OUT <= 0 ; 
