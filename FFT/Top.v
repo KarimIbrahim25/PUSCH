639be9fe619daf62e6babab6ec234e996fe19b3d
Mostafa sakr <mostafasakr01@hotmail.com>
2024-06-22 01:47:44 +0300

    Update Top.v

--- FFT/Top.v
+++ FFT/Top.v
@@ -170,8 +170,31 @@ reg [7:0] fft_from5_to3_counter;
 
 wire pulse3;
 
-wire done;
+reg done;
 
+reg [2:0] edge_counter;
+
+always @(posedge clk or posedge rst) begin
+    if(rst) begin
+        edge_counter <= 0;
+        done <= 0;
+    end 
+
+    else if (edge_counter == 3) begin
+        edge_counter <= 0;
+        done <= done_slow;
+    end
+
+    else if (done_slow) begin
+        edge_counter <= edge_counter + 1;
+        done <= 0;
+    end
+
+    else begin
+        edge_counter <= 0;
+        done <= 0;
+    end
+end
 always @(posedge clk or posedge rst ) begin
     
         if(rst) begin
